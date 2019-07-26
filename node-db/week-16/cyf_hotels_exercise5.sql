813705f2175e7e806b0620cd9ab7978f64c51efb
Luke Taylor <tekul.hs@gmail.com>
2019-07-26 15:17:45 +0100

    Fix whitespace in sql files
    Removed tabs as they prevent the commands from working if the text is pasted
    directly into psql instead of run as a file.
    
    The file also renders better when viewed on github.

--- node-db/week-16/cyf_hotels_exercise5.sql
+++ node-db/week-16/cyf_hotels_exercise5.sql
@@ -3,28 +3,28 @@ drop table if exists hotels;
 drop table if exists customers;
 
 CREATE TABLE customers (
-  id 		SERIAL PRIMARY KEY,
-  name 		VARCHAR(30) NOT NULL,
-  email 	VARCHAR(120) NOT NULL,
-  address 	VARCHAR(120),
-  city 		VARCHAR(30),
-  postcode 	VARCHAR(12),
-  country 	VARCHAR(20)
+  id       SERIAL PRIMARY KEY,
+  name     VARCHAR(30) NOT NULL,
+  email    VARCHAR(120) NOT NULL,
+  address  VARCHAR(120),
+  city     VARCHAR(30),
+  postcode VARCHAR(12),
+  country  VARCHAR(20)
 );
 
 CREATE TABLE hotels (
-	id			SERIAL PRIMARY KEY,
-	name		VARCHAR(120) NOT NULL,
-	rooms	    INT NOT NULL,
-	postcode    VARCHAR(10)
+  id       SERIAL PRIMARY KEY,
+  name     VARCHAR(120) NOT NULL,
+  rooms    INT NOT NULL,
+  postcode VARCHAR(10)
 );
 
 CREATE TABLE bookings (
-	id					SERIAL PRIMARY KEY,
-	customer_id 		INT REFERENCES customers(id),
-	hotel_id			INT REFERENCES hotels(id),
-	checkin_date 		DATE NOT NULL,
-	nights				INT NOT NULL
+  id            SERIAL PRIMARY KEY,
+  customer_id   INT REFERENCES customers(id),
+  hotel_id      INT REFERENCES hotels(id),
+  checkin_date  DATE NOT NULL,
+  nights        INT NOT NULL
 );
 
 INSERT INTO customers (name, email, address, city, postcode, country) VALUES ('John Smith','j.smith@johnsmith.org','11 New Road','Liverpool','L10 2AB','UK');
--- node-db/week-17/cyf_ecommerce.sql
+++ node-db/week-17/cyf_ecommerce.sql
@@ -5,31 +5,31 @@ drop table if exists products;
 drop table if exists suppliers;
 
 CREATE TABLE customers (
-  id 		    SERIAL PRIMARY KEY,
-  name 	        VARCHAR(50) NOT NULL,
-  address 	    VARCHAR(120),
-  city 		    VARCHAR(30),
-  country 	    VARCHAR(20)
+  id       SERIAL PRIMARY KEY,
+  name     VARCHAR(50) NOT NULL,
+  address  VARCHAR(120),
+  city     VARCHAR(30),
+  country  VARCHAR(20)
 );
 
 CREATE TABLE suppliers (
     id              SERIAL PRIMARY KEY,
-    supplier_name    VARCHAR(100) NOT NULL,
+    supplier_name   VARCHAR(100) NOT NULL,
     country         VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE products (
-	id			    SERIAL PRIMARY KEY,
-	product_name	    VARCHAR(100) NOT NULL,
-	unit_price	    INT NOT NULL,
-	supplier_id     INT REFERENCES suppliers(id)
+    id             SERIAL PRIMARY KEY,
+    product_name   VARCHAR(100) NOT NULL,
+    unit_price     INT NOT NULL,
+    supplier_id    INT REFERENCES suppliers(id)
 );
 
 CREATE TABLE orders (
-	id				SERIAL PRIMARY KEY,
-	order_date 		DATE NOT NULL,
+    id              SERIAL PRIMARY KEY,
+    order_date      DATE NOT NULL,
     order_reference VARCHAR(10) NOT NULL,
-	customer_id		INT REFERENCES customers(id)
+    customer_id     INT REFERENCES customers(id)
 );
 
 CREATE TABLE order_items (
